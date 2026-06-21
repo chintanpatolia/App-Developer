1c78d1bb02b5344026fb6c1e0889ea49144fe08b
CP <chintanpatolia@gmail.com>
2026-06-21 11:17:17 -0500

    Phase 35B Batch 3: add 12 new breakfast recipes to BreakfastRecipes2.kt

--- app/src/main/java/com/dailyhealthcoach/ui/recipes/RecipeCatalog.kt
+++ app/src/main/java/com/dailyhealthcoach/ui/recipes/RecipeCatalog.kt
@@ -2,6 +2,7 @@ package com.dailyhealthcoach.ui.recipes
 
 import com.dailyhealthcoach.ui.recipes.catalog.BoosterRecipes
 import com.dailyhealthcoach.ui.recipes.catalog.BreakfastRecipes
+import com.dailyhealthcoach.ui.recipes.catalog.BreakfastRecipes2
 import com.dailyhealthcoach.ui.recipes.catalog.DinnerRecipes
 import com.dailyhealthcoach.ui.recipes.catalog.LunchRecipes
 import com.dailyhealthcoach.ui.recipes.catalog.SnackRecipes
@@ -10,6 +11,7 @@ object RecipeCatalog {
 
     val ALL: List<Recipe> =
         BreakfastRecipes.ALL +
+        BreakfastRecipes2.ALL +
         LunchRecipes.ALL +
         DinnerRecipes.ALL +
         SnackRecipes.ALL +
