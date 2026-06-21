bfabe650d0bf967f7652096f96181cfd0e6f51b1
CP <chintanpatolia@gmail.com>
2026-06-21 11:21:37 -0500

    Phase 35B Batch 5: add 12 new dinner recipes to DinnerRecipes2.kt

--- app/src/main/java/com/dailyhealthcoach/ui/recipes/RecipeCatalog.kt
+++ app/src/main/java/com/dailyhealthcoach/ui/recipes/RecipeCatalog.kt
@@ -4,6 +4,7 @@ import com.dailyhealthcoach.ui.recipes.catalog.BoosterRecipes
 import com.dailyhealthcoach.ui.recipes.catalog.BreakfastRecipes
 import com.dailyhealthcoach.ui.recipes.catalog.BreakfastRecipes2
 import com.dailyhealthcoach.ui.recipes.catalog.DinnerRecipes
+import com.dailyhealthcoach.ui.recipes.catalog.DinnerRecipes2
 import com.dailyhealthcoach.ui.recipes.catalog.LunchRecipes
 import com.dailyhealthcoach.ui.recipes.catalog.LunchRecipes2
 import com.dailyhealthcoach.ui.recipes.catalog.SnackRecipes
@@ -16,6 +17,7 @@ object RecipeCatalog {
         LunchRecipes.ALL +
         LunchRecipes2.ALL +
         DinnerRecipes.ALL +
+        DinnerRecipes2.ALL +
         SnackRecipes.ALL +
         BoosterRecipes.ALL
 }
